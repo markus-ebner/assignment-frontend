d4dbd8f805270df707a25c9062cad76a227d554b
Markus Ebner <mebner.mmt-b2015@fh-salzburg.ac.at>
2016-11-12 23:25:30 +0100

    untested version of module.js

--- 01-modules/src/module.js
+++ 01-modules/src/module.js
@@ -1,15 +1,25 @@
 export function valid(email) {
-
+  var pattern = /[a-z].mmt-[bm][0-9]{4}@fh-salzburg.ac.at/;
+  return pattern.test(email)
 }
 
 export function degreeProgram(email) {
-
+  var pattern = /[a-z]{3}(?=-[bm][0-9]{4}@)/;
+  return pattern.exec(email)[0].toUpperCase();
 }
 
 export function level(email) {
-
+  var pattern = /[bm](?=[0-9]{4}@)/;
+  return pattern.exec(email)[0].toUpperCase() + 'A';
 }
 
 export function graduationYear(email) {
-
+  var pattern = /[0-9]{4}(?=@)/;
+  var emailyear = pattern.exec(email)[0];
+  if(level(email) == 'BA')
+    return emailyear += 3;
+  else
+    return emailyear += 2;
 }
+
+
